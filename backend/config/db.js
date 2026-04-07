cff3cec3c59e7a32438805f7c5ce8895cbaba51f
Suyashjain028 <b241373@skit.ac.in>
2026-04-07 10:41:18 +0530

    Update db.js

--- backend/config/db.js
+++ backend/config/db.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 
 const connectDB = async () => {
   try {
-    const conn = await mongoose.connect(process.env."mongodb+srv://blackapp:Blackapp1941@cluster0.4rgshxc.mongodb.net/navkar_donations");
+    const conn = await mongoose.connect(process.env.MONGO_URI);
     console.log(`MongoDB Connected: ${conn.connection.host}`);
   } catch (error) {
     console.error(`Error: ${error.message}`);
